3544f65d3f0fb4fefd1fa7ca4a3af7198c396f6b
naikgitacc <naiknashwini@gmail.com>
2023-09-27 17:42:02 +0530

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ class HelloWorld
 	public static void main(String args[])
 	{
 		System.out.println("Hi Java Developers !!");
-		System.out.println("Welcome to Pro-Wings!!");
+		System.out.println("Welcome to Git!!");
 	}
 }
